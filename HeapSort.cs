e68f501e7d35765047e184d02de78b781809818a
profounddark <39137591+profounddark@users.noreply.github.com>
2023-05-09 11:24:53 -0700

    first commit

---
+++ HeapSort.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+class HeapSort
+{
+
+
+    /*
+        0
+        1               2
+        3       4       5       6
+        7   8   9   10  11  12  13  14
+
+    */
+
+    // assume all children are heapified, heapify
+    // the current node
+    // probably requires a sift down
+
+    // children are 2n+1 and 2n+2
+
+
+    /// <summary>
+    /// Swap two integer values (by reference!).
+    /// </summary>
+    /// <param name="first">the first integer to swap</param>
+    /// <param name="second">the second integer to swap</param>
+    public static void Swap(ref int first, ref int second)
+    {
+        int temp = first;
+        first = second;
+        second = temp;
+    }
+
+
+    /// <summary>
+    /// SiftDown sifts a node down in the Max Heap.  This assumes that all
+    /// values in the array after the give node are already in Max Heap
+    /// configuration.
+    /// </summary>
+    /// <param name="heap">the heap-in-progress</param>
+    /// <param name="node">the particular node to "sift down"</param>
+    public static void SiftDown(int[] heap, int node, int last)
+    {
+        int left = (node * 2) + 1;
+        int right = (node * 2) + 2;
+
+        int biggest = node;
+
+        if ((left < last) && (heap[left] > heap[biggest]))
+        {
+            biggest = left;
+        }
+
+        if ((right < last) && (heap[right] > heap[biggest]))
+        {
+            biggest = right;
+        }
+
+        if (node != biggest)
+        {
+            Swap(ref heap[biggest], ref heap[node]);
+            SiftDown(heap, biggest, last);
+        }
+
+    }
+
+    /// <summary>
+    /// HeapifyMax takes an array and arranges it into a Max Heap.
+    /// </summary>
+    /// <param name="newHeap">the array to Heapify</param>
+    public static void HeapifyMax(int[] newHeap)
+    {
+        for (int i = (newHeap.Length / 2); i >= 0; i--)
+        {
+            SiftDown(newHeap, i, newHeap.Length);
+        }
+    }
+
+    public static void Sort(int[] sortArray)
+    {
+        HeapifyMax(sortArray);
+        for (int i = sortArray.Length - 1; i > 0; i--)
+        {
+            Swap(ref sortArray[0], ref sortArray[i]);
+            SiftDown(sortArray, 0, i);
+        }
+    }
+
+    /// <summary>
+    /// PrintArray prints out the contents of an integer array.
+    /// </summary>
+    /// <param name="printArray">the array to be printed</param>
+    public static void PrintArray(int[] printArray)
+    {
+        Console.WriteLine("Contents of Array:");
+        Console.Write("{ ");
+        for (int i = 0; i < printArray.Length; i++)
+        {
+            Console.Write(printArray[i]);
+            if (i < printArray.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine(" }");
+    }
+
+    public static void TestSort(int[] testArray)
+    {
+        Console.WriteLine("Testing HeapSort on following array:");
+        PrintArray(testArray);
+        Console.WriteLine("Sorting...");
+        Sort(testArray);
+        Console.WriteLine("Array after executing HeapSort:");
+        PrintArray(testArray);
+    }
+
+    public static void Main(string[] args)
+    {
+        // gotta test my heaps
+        int[] newHeap1 = { 40, 1, 2, 91, 43, 5, 6, 63, 8, 9, 28, 18, 12, 31, 14 };
+
+        TestSort(newHeap1);
+
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,32 @@
+# CPSC 5031 Homework 5 (5 pts)
+
+## Heap Sort
+
+Let’s write more code!
+Implement HeapSort per the algorithm\* to sort an array A in ascending order.
+
+```
+Which algorithm? You pick. A big part of “algorithms” is knowing how
+to convert mathy pseudo-code into your language of choice. So use the
+textbook, use the slides, use the Internet... just pick one and run with it.
+```
+
+Here’s what you need to turn in:
+
+1. Where you got the algorithm from
+2. Your code
+3. The output of your code
+4. An explanation of why you chose the test cases you did
+
+## FAQ
+
+1. How many test cases do I need? You decide. It’s your code, so you should best know
+   where things might go wrong. Add test cases that explore each of these places where
+   something could break.
+2. Can I get the algorithm off Wikipedia? Yes. It just has to implement HeapSort.
+3. Are HeapSort and insertion into a Heap the same thing? NO. They are most definitely
+   not the same thing! Make sure you’re doing HeapSort!
+4. Why are you making us write so much code that you said will just be in a library
+   anyway? Because the best way to get better at coding is to write more code. That’s
+   why I say “Let’s write more code!” in every homework and on the tests. Keep doing it
+   and you’ll get just a little bit better every time. 👍
